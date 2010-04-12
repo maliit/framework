0c70bc9cc7edc9d27b67658a74f8c38cb615dda5
Raymond Liu <raymond.liu@intel.com>
2010-04-12 15:54:07 +0800

    rename project to dui-im-client

--- gtk-input-context/src/qt-keysym-map.cpp
+++ gtk-input-context/src/qt-keysym-map.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2001 Ellis Whitehead <ellis@kde.org>
+ * Copyright (C) 2010, Intel Corporation.
+ *
+ * Author: Raymond Liu <raymond.liu@intel.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
 
 #include <X11/keysym.h>
 #include <qstring.h>
