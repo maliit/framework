maliit/framework

commit f8f7908ea575ab5909d52f36f3bcb945a719e2de

Fixes: NB#184715
RevBy: Jarno Malmari
Details: Add a flag to MIMPluginManager so that region updates are discarded
while we don't want to show any plugin.

src/mimpluginmanager.cpp
@@ -66,7 +66,8 @@ MIMPluginManagerPrivate::MIMPluginManagerPrivate(MInputContextConnection *connec
       imAccessoryEnabledConf(0),
       settingsDialog(0),
       adaptor(0),
-      connectionValid(false)
+      connectionValid(false),
+      acceptRegionUpdates(true)
 {
 }
 
@@ -602,6 +603,8 @@ void MIMPluginManagerPrivate::initActiveSubView()
 
 void MIMPluginManagerPrivate::showActivePlugins()
 {
+    acceptRegionUpdates = true;
+
     foreach (MInputMethodPlugin *plugin, activePlugins) {
         plugins[plugin].inputMethod->show();
     }
@@ -613,9 +616,12 @@ void MIMPluginManagerPrivate::hideActivePlugins()
         // disappear the settings dialog without animation before hiding active plugins.
         MPlainWindow::instance()->sceneManager()->disappearSceneWindowNow(settingsDialog);
     }
+
     foreach (MInputMethodPlugin *plugin, activePlugins) {
         plugins[plugin].inputMethod->hide();
     }
+
+    acceptRegionUpdates = false;
 }
 
 QMap<QString, QString> MIMPluginManagerPrivate::availableSubViews(const QString &plugin, MIMHandlerState state) const
@@ -895,11 +901,15 @@ void MIMPluginManager::showInputMethodSettings()
 void MIMPluginManager::updateRegion(const QRegion &region)
 {
     Q_D(MIMPluginManager);
-    // record input method object's region.
-    if (d->activeImRegion != region)
+    // Record input method object's region.
+    if (d->activeImRegion != region) {
         d->activeImRegion = region;
-    if (!d->settingsDialog || !d->settingsDialog->isVisible()) {
-        //if settings dialog is visible, don't update region.
+    }
+
+    // If settings dialog is visible, don't update region. Don't update region
+    // when no region updates from the plugin side are expected.
+    if (d->acceptRegionUpdates &&
+        (!d->settingsDialog || !d->settingsDialog->isVisible())) {
         emit regionUpdated(region);
     }
 }

src/mimpluginmanager_p.h
@@ -127,6 +127,8 @@ public:
 
     MIMPluginManager *q_ptr;
     bool connectionValid;
+
+    bool acceptRegionUpdates;
 };
 
 class MIMPluginManagerAdaptor: public QDBusAbstractAdaptor

commit ebe2de584dcaadf94bb82955801391ad6b154665

Fixes: Update region after hiding plugins
RevBy: TrustMe
Details: f8f7908e introduced a nasty regression - once a plugin is
requested to hide, it might delay its region update until after animations etc.
have finished. But at that point, the plugin manager isn't accepting those
region updates any longer, thus leaving us with an invisible VKB region on the
display. This commit explicity resets the region after hiding plugins.

src/mimpluginmanager.cpp
@@ -603,8 +603,6 @@ void MIMPluginManagerPrivate::initActiveSubView()
 
 void MIMPluginManagerPrivate::showActivePlugins()
 {
-    acceptRegionUpdates = true;
-
     foreach (MInputMethodPlugin *plugin, activePlugins) {
         plugins[plugin].inputMethod->show();
     }
@@ -620,8 +618,6 @@ void MIMPluginManagerPrivate::hideActivePlugins()
     foreach (MInputMethodPlugin *plugin, activePlugins) {
         plugins[plugin].inputMethod->hide();
     }
-
-    acceptRegionUpdates = false;
 }
 
 QMap<QString, QString> MIMPluginManagerPrivate::availableSubViews(const QString &plugin, MIMHandlerState state) const
@@ -916,13 +912,23 @@ void MIMPluginManager::updateRegion(const QRegion &region)
 void MIMPluginManager::showActivePlugins()
 {
     Q_D(MIMPluginManager);
+
+    d->acceptRegionUpdates = true;
     d->showActivePlugins();
 }
 
 void MIMPluginManager::hideActivePlugins()
 {
     Q_D(MIMPluginManager);
+
     d->hideActivePlugins();
+
+    // Do not accept region updates from hidden plugins. Emit an empty region
+    // update, because we cannot assume that a plugin sends a region update
+    // immediately after hiding it. It could play some animation, for example,
+    // and only then send the region update (which would then be ignored).
+    d->acceptRegionUpdates = false;
+    emit regionUpdated(QRegion());
 }
 
 QMap<QString, QString> MIMPluginManager::availableSubViews(const QString &plugin, MIMHandlerState state) const