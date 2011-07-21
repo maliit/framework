1cf73db762004be6d97edf0f1166ba327bf61f63
Jan Arne Petersen <jpetersen@openismus.com>
2011-07-21 21:29:23 +0200

    Add EventSpyInputContext to test utils
    Adds a new EventSpyInputContext input context to test utils which allows
    to track events from QInputContext::filterEvent.

--- tests/utils/utils.h
+++ tests/utils/utils.h
@@ -17,7 +17,11 @@
 #ifndef UTILS_H__
 #define UTILS_H__
 
+#include <QInputContext>
 #include <QWidget>
+
+#include <tr1/functional>
+#include <vector>
 
 namespace MaliitTestUtils {
     bool isTestingInSandbox();
@@ -28,6 +32,29 @@ namespace MaliitTestUtils {
         explicit RemoteWindow(QWidget *p = 0, Qt::WindowFlags f = 0);
 
         void paintEvent(QPaintEvent *);
+    };
+
+    template <typename T>
+    class EventSpyInputContext : public QInputContext, public std::vector<T>
+    {
+    public:
+        typedef std::tr1::function<T (const QEvent *event)> TransformFunction;
+
+        EventSpyInputContext(TransformFunction newTransform) : transform(newTransform) {}
+
+        virtual QString identifierName() { return QString::fromLatin1("EventSpyInputContext"); }
+        virtual bool isComposing() const { return false; }
+        virtual QString language() { return QString::fromLatin1("EN"); }
+        virtual void reset() {}
+
+    protected:
+        virtual bool filterEvent(const QEvent *event) {
+            push_back(transform(event));
+            return true;
+        }
+
+    private:
+        const TransformFunction transform;
     };
 }
 
