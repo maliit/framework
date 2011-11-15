maliit/framework

commit 028ec19f65972b01cdf6340a304575baba3eff9c

New: Add normalization for dynamic QObject property names
RevBy: Jon Nordby
Maliit uses dynamic QObject properties to allow integration with Maliit even if
libmaliit cannot be used directly.
The property names usually contain hyphens, which causes problem when mapping
QML properties to QObject properties.  The canonical form for QML poperties
uses CamelCase, for example. Now with propery name normalization,
"customPropertyName" will be mapped to "maliit-custom-property-name",
eventually.

input-context/minputcontext.cpp
@@ -52,6 +52,45 @@ namespace
     const char * const ToolbarTarget("/toolbar");
     const char * const InputContextName(MALIIT_INPUTCONTEXT_NAME);
 
+    //! Extracts a dynamic QObject property from obj, using normalization rules
+    //! for propertyName (eases mapping of QML properties to QObject
+    //! properties). Valid property names start with "maliiit-"
+    QVariant extractProperty(const QObject *obj,
+                             const char * const propertyName)
+    {
+        static const QByteArray prefix("maliit-");
+        const QByteArray name(propertyName);
+
+        if (not obj || not name.startsWith(prefix)) {
+            qCritical() << __PRETTY_FUNCTION__
+                        << "Ignoring invalid object or invalid propertyName "
+                        << propertyName;
+            return QVariant();
+        }
+
+        QVariant result = obj->property(name);
+
+        if (not result.isValid()) {
+            QByteArray camelCased;
+            bool turnUp = false;
+
+            // Function guard guarantees that properyName starts with "maliit-".
+            for (int idx = prefix.length(); idx < name.length(); ++idx) {
+                const QChar curr = name.at(idx);
+                if (curr == '-') {
+                    turnUp = true;
+                } else {
+                    camelCased.append(turnUp ? curr.toUpper() : curr);
+                    turnUp = false;
+                }
+            }
+
+            result = obj->property(camelCased);
+        }
+
+        return result;
+    }
+
 #ifdef Q_WS_X11
     enum {
         XKeyPress = KeyPress,
@@ -1028,16 +1067,16 @@ QMap<QString, QVariant> MInputContext::getStateInformation() const
         stateInformation["visualizationPriority"] = queryResult.toBool();
     }
 
-    queryResult = focused->property(Maliit::InputMethodQuery::attributeExtensionId);
+    queryResult = extractProperty(focused, Maliit::InputMethodQuery::attributeExtensionId);
 
     if (!queryResult.isValid()) {
         if (focusedObject) {
             // FIXME: Kill off once users of this property were able to switch
             // to Maliit::InputMethodQuery::attributeExtensionId.
-            queryResult = focusedObject->property("meego-inputmethod-attribute-extension-id");
+            queryResult = extractProperty(focusedObject, "meego-inputmethod-attribute-extension-id");
 
             if (!queryResult.isValid()) {
-                queryResult = focusedObject->property(Maliit::InputMethodQuery::attributeExtensionId);
+                queryResult = extractProperty(focusedObject, Maliit::InputMethodQuery::attributeExtensionId);
             }
         }
 
@@ -1052,7 +1091,7 @@ QMap<QString, QVariant> MInputContext::getStateInformation() const
     }
 
     // toolbar file
-    queryResult = focused->property(Maliit::InputMethodQuery::attributeExtension);
+    queryResult = extractProperty(focused, Maliit::InputMethodQuery::attributeExtension);
 
     if (!queryResult.isValid())
     {
@@ -1154,12 +1193,13 @@ QMap<QString, QVariant> MInputContext::getStateInformation() const
     // 3. focusWidget()->inputMethodQuery(.),
     // Ensures that plain Qt and Qt Comoponents can readily use this override,
     // as neither of them implement a Maliit-specific IM query.
-    queryResult = focused->property(Maliit::InputMethodQuery::westernNumericInputEnforced);
+    queryResult = extractProperty(focused, Maliit::InputMethodQuery::westernNumericInputEnforced);
 
     if (!queryResult.isValid())
     {
        if (focusedObject) {
-            queryResult = focusedObject->property(Maliit::InputMethodQuery::westernNumericInputEnforced);
+            queryResult = extractProperty(focusedObject,
+                                          Maliit::InputMethodQuery::westernNumericInputEnforced);
         }
 
         if (!queryResult.isValid()) {
@@ -1170,12 +1210,12 @@ QMap<QString, QVariant> MInputContext::getStateInformation() const
 
     stateInformation[Maliit::InputMethodQuery::westernNumericInputEnforced] = queryResult.toBool();
 
-    queryResult = focused->property(Maliit::InputMethodQuery::translucentInputMethod);
+    queryResult = extractProperty(focused, Maliit::InputMethodQuery::translucentInputMethod);
 
     if (!queryResult.isValid())
     {
        if (focusedObject) {
-            queryResult = focusedObject->property(Maliit::InputMethodQuery::translucentInputMethod);
+            queryResult = extractProperty(focusedObject, Maliit::InputMethodQuery::translucentInputMethod);
         }
     }
 

tests/ut_minputcontext/ut_minputcontext.cpp
@@ -894,7 +894,7 @@ void Ut_MInputContext::testMPreeditInjectionEventCompatibility()
 #endif
 }
 
-void Ut_MInputContext::testPropertyNormalization_data()
+void Ut_MInputContext::testPropertyNameNormalization_data()
 {
     typedef QByteArray QBA;
     typedef QVariant QV;
@@ -902,21 +902,30 @@ void Ut_MInputContext::testPropertyNormalization_data()
     QTest::addColumn<QBA>("name");
     QTest::addColumn<QBA>("defaultName");
     QTest::addColumn<QVariant>("value");
-
-    QCOMPARE(Maliit::InputMethodQuery::westernNumericInputEnforced,
-             "maliit-western-numeric-input-enforced");
+    QTest::addColumn<QVariant>("expected");
 
     QTest::newRow("no normalization")
      << QBA("maliit-western-numeric-input-enforced")
-     << QBA("maliit-western-numeric-input-enforced")
-     << QV(true);
+     << QBA(Maliit::InputMethodQuery::westernNumericInputEnforced)
+     << QV(true) << QV(true);
+
+    QTest::newRow("Qt component normalization")
+     << QBA("westernNumericInputEnforced")
+     << QBA(Maliit::InputMethodQuery::westernNumericInputEnforced)
+     << QV(true) << QV(true);
+
+    QTest::newRow("Invalid property")
+     << QBA("invalidPropertyName")
+     << QBA("invalid-property-name")
+     << QV(42) << QV();
 }
 
-void Ut_MInputContext::testPropertyNormalization()
+void Ut_MInputContext::testPropertyNameNormalization()
 {
     QFETCH(QByteArray, name);
     QFETCH(QByteArray, defaultName);
     QFETCH(QVariant, value);
+    QFETCH(QVariant, expected);
 
     WidgetStub widget(0);
     gFocusedWidget = &widget;
@@ -926,8 +935,7 @@ void Ut_MInputContext::testPropertyNormalization()
     m_subject->update();
     QVariant extracted = m_subject->getStateInformation().value(QString(defaultName));
 
-    QCOMPARE(extracted.isValid(), value.isValid());
-    QCOMPARE(extracted, value);
+    QCOMPARE(extracted, expected);
 }
 
 

tests/ut_minputcontext/ut_minputcontext.h
@@ -173,8 +173,8 @@ private Q_SLOTS:
 
     void testMPreeditInjectionEventCompatibility();
 
-    void testPropertyNormalization_data();
-    void testPropertyNormalization();
+    void testPropertyNameNormalization_data();
+    void testPropertyNameNormalization();
 
 private:
     void waitAndProcessEvents(int waitTime);

commit 35fb7e55389fc8f13e08ebe45f52a07dfd9e79c1

Fixes: NB#291062 - Regression: QML Components Gallery, Text Input, Sip Attributes Example failed.
RevBy: Jan Arne Petersen
Regression introduced by 028ec19 - "New: Add normalization for dynamic QObject
property names", fixed by ignoring normalization for deprecated property names.

input-context/minputcontext.cpp
@@ -1085,7 +1085,7 @@ QMap<QString, QVariant> MInputContext::getStateInformation() const
         if (focusedObject) {
             // FIXME: Kill off once users of this property were able to switch
             // to Maliit::InputMethodQuery::attributeExtensionId.
-            queryResult = extractProperty(focusedObject, "meego-inputmethod-attribute-extension-id");
+            queryResult = focusedObject->property("meego-inputmethod-attribute-extension-id");
 
             if (!queryResult.isValid()) {
                 queryResult = extractProperty(focusedObject, Maliit::InputMethodQuery::attributeExtensionId);