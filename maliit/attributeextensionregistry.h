02da8bc7b7a46abd073f225b7c6a07ec5e6664a1
David King <davidk@openismus.com>
2011-11-16 16:42:00 +0100

    Hide AttributeExtensionRegistry from documentation
    RevBy: Michael Hasselmann

--- maliit/attributeextensionregistry.h
+++ maliit/attributeextensionregistry.h
@@ -32,6 +32,7 @@ class AttributeExtensionRegistryPrivate;
 typedef QList<QWeakPointer<AttributeExtension> > ExtensionList;
 
 //! \ingroup libmaliit
+//! \internal
 class AttributeExtensionRegistry : public QObject
 {
     Q_OBJECT
