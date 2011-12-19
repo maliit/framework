e7f835eebec36409636d4abf425b90937db61da5
Carlos Martín <cmartin@liberalia.net>
2011-12-19 21:01:23 +0100

    Partial: Maliit#14 - Maliit requires glib/gio 2.26+

--- input-context/inputcontextdbusaddress.cpp
+++ input-context/inputcontextdbusaddress.cpp
@@ -16,32 +16,48 @@
 
 #include "inputcontextdbusaddress.h"
 
+#ifdef MALIIT_USE_GIO_API
 #include <gio/gio.h>
+#else
+#include <glib-object.h>
+#include <dbus/dbus-glib.h>
+#endif
 #include <tr1/memory>
 #include <tr1/functional>
 #include <QDebug>
 
 namespace {
-    const char * const MaliitServerName = "org.maliit.server";
-    const char * const MaliitServerObjectPath = "/org/maliit/server/address";
-    const char * const MaliitServerInterface = "org.maliit.Server.Address";
-    const char * const MaliitServerAddressProperty = "address";
+const char * const MaliitServerName = "org.maliit.server";
+const char * const MaliitServerObjectPath = "/org/maliit/server/address";
+const char * const MaliitServerInterface = "org.maliit.Server.Address";
+const char * const MaliitServerAddressProperty = "address";
 
-    template<typename T>
-    class SafeUnref {
-    public:
-        explicit SafeUnref(std::tr1::function<void (T*)>unref)
-            : mUnref(unref)
-        {}
+#ifndef MALIIT_USE_GIO_API
+const char * const DBusPropertiesInterface = "org.freedesktop.DBus.Properties";
+const char * const DBusPropertiesGetMethod = "Get";
+#endif
 
-        void operator()(T* t)
+    struct SafeUnref
+    {
+#ifdef MALIIT_USE_GIO_API
+        static inline void cleanup(GDBusProxy *pointer)
         {
-            if (t)
-                mUnref(t);
+            g_object_unref(pointer);
         }
-
-    private:
-        std::tr1::function<void (T*)> mUnref;
+        static inline void cleanup(GVariant *pointer)
+        {
+            g_variant_unref(pointer);
+        }
+#else
+        static inline void cleanup(DBusGConnection *pointer)
+        {
+            dbus_g_connection_unref(pointer);
+        }
+        static inline void cleanup(DBusGProxy *pointer)
+        {
+            g_object_unref(pointer);
+        }
+#endif
     };
 }
 
@@ -53,36 +69,78 @@ Address::Address()
 {
 }
 
+#ifdef MALIIT_USE_GIO_API
 const std::string Address::get() const
 {
     GError *error = NULL;
-    std::tr1::shared_ptr<GDBusProxy> proxy(g_dbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION,
-                                                                         G_DBUS_PROXY_FLAGS_NONE,
-                                                                         0,
-                                                                         MaliitServerName,
-                                                                         MaliitServerObjectPath,
-                                                                         MaliitServerInterface,
-                                                                         0, &error),
-                                           SafeUnref<GDBusProxy>(g_object_unref));
 
-    if (!proxy) {
+    QScopedPointer<GDBusProxy, SafeUnref>
+            proxy(g_dbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION,
+                                                G_DBUS_PROXY_FLAGS_NONE,
+                                                0,
+                                                MaliitServerName,
+                                                MaliitServerObjectPath,
+                                                MaliitServerInterface,
+                                                0, &error));
+
+    if (proxy.isNull()) {
         qWarning() << __PRETTY_FUNCTION__ << error->message;
         g_error_free(error);
         return std::string();
     }
 
-    std::tr1::shared_ptr<GVariant> address(g_dbus_proxy_get_cached_property(proxy.get(), MaliitServerAddressProperty),
-                                           SafeUnref<GVariant>(g_variant_unref));
+    QScopedPointer<GVariant, SafeUnref>
+            address(g_dbus_proxy_get_cached_property(proxy.data(),
+                                                     MaliitServerAddressProperty));
 
-    if (!address) {
+    if (address.isNull()) {
         return std::string();
     }
 
-    std::string result(g_variant_get_string(address.get(), 0));
+    std::string result(g_variant_get_string(address.data(), 0));
 
     return result;
 }
+#else
+const std::string Address::get() const
+{
+    GValue value = {0,};
+    GError *error = NULL;
+    
+    QScopedPointer<DBusGConnection, SafeUnref>
+            connection(dbus_g_bus_get(DBUS_BUS_SESSION, &error));
 
+    if (connection.isNull()) {
+        qWarning() << __PRETTY_FUNCTION__ << error->message;
+        g_error_free(error);
+        return std::string();
+    }
+
+    QScopedPointer<DBusGProxy, SafeUnref>
+            proxy(dbus_g_proxy_new_for_name(connection.data(),
+                                            MaliitServerName,
+                                            MaliitServerObjectPath,
+                                            DBusPropertiesInterface));
+
+    if (!dbus_g_proxy_call(proxy.data(),
+                           DBusPropertiesGetMethod,
+                           &error,
+                           G_TYPE_STRING, MaliitServerInterface,
+                           G_TYPE_STRING, MaliitServerAddressProperty,
+                           G_TYPE_INVALID,
+                           G_TYPE_VALUE, &value, G_TYPE_INVALID)) {
+
+        qWarning() << __PRETTY_FUNCTION__ << error->message;
+        g_error_free(error);
+        return std::string();
+    }
+
+    std::string result(g_value_get_string(&value));
+    g_value_unset(&value);
+
+    return result;
+}
+#endif
 } // namespace DBus
 } // namespace InputContext
 } // namespace Maliit
