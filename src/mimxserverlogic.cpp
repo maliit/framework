4e1cbd240bf5d5bb9c6c7266031f561da7df22ce
Jon Nordby <jonn@openismus.com>
2012-02-14 19:23:03 +0100

    New: MImXServerLogic, server logic for standalone X11 mode
    RevBy: Michael Hasselmann, Jan Arne Petersen
    Moves out everything that does not require being in
    a QApplication subclass from MImXApplication.

---
+++ src/mimxserverlogic.cpp
@@ -0,0 +1,266 @@
+/* * This file is part of meego-im-framework *
+ *
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ * Contact: Nokia Corporation (directui@nokia.com)
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Nokia at directui@nokia.com.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPL included in the packaging
+ * of this file.
+ */
+
+#include "mimxserverlogic.h"
+
+#include "mpassthruwindow.h"
+#include "mimremotewindow.h"
+#include "mimxextension.h"
+#include "mimrotationanimation.h"
+
+#include "mimxapplication.h"
+#include "mimpluginsproxywidget.h"
+#include "mimrotationanimation.h"
+#include "mimserveroptions.h"
+
+#include <QDebug>
+
+#ifdef HAVE_MEEGOGRAPHICSSYSTEM
+#include <QtMeeGoGraphicsSystemHelper/QMeeGoGraphicsSystemHelper>
+#endif
+
+#include <X11/Xlib.h>
+
+namespace
+{
+    class ConfigureForCompositing {
+
+    public:
+        ConfigureForCompositing(const MImServerXOptions &newOptions)
+            : options(newOptions)
+        {
+        }
+
+        bool operator()(QWidget *w)
+        {
+            if (not w) {
+                return false;
+            }
+
+            w->setAttribute(Qt::WA_OpaquePaintEvent);
+            w->setAttribute(Qt::WA_NoSystemBackground);
+            w->setAutoFillBackground(false);
+            // Be aware that one cannot verify whether the background role *is*
+            // QPalette::NoRole - see QTBUG-17924.
+            w->setBackgroundRole(QPalette::NoRole);
+
+            if (!options.selfComposited) {
+                // Careful: This flag can trigger a call to
+                // qt_x11_recreateNativeWidgetsRecursive
+                // - which will crash when it tries to get the effective WId
+                // (as none of widgets have been mapped yet).
+                w->setAttribute(Qt::WA_TranslucentBackground);
+            }
+
+            return true;
+        }
+
+    private:
+        const MImServerXOptions &options;
+
+    };
+
+
+}
+
+MImXServerLogic::MImXServerLogic(const MImServerXOptions &options, QObject *parent) :
+    MImAbstractServerLogic(parent),
+    xOptions(options),
+    mBackgroundSuppressed(false),
+    mCompositeExtension(),
+    mDamageExtension(),
+    mPassThruWindow(),
+    mRemoteWindow()
+{
+    mPassThruWindow.reset(new MPassThruWindow(this, xOptions));
+    mPluginsProxyWidget.reset(new MImPluginsProxyWidget(mPassThruWindow.data()));
+    mRotationAnimation = new MImRotationAnimation(pluginsProxyWidget(), passThruWindow(),
+                                                  this, xOptions);
+
+#ifdef HAVE_MEEGOGRAPHICSSYSTEM
+    QMeeGoGraphicsSystemHelper::setSwitchPolicy(QMeeGoGraphicsSystemHelper::NoSwitch);
+#endif
+
+    configureWidgetsForCompositing();
+}
+
+MImXServerLogic::~MImXServerLogic()
+{
+}
+
+void MImXServerLogic::finalize()
+{
+    // Cannot destroy QWidgets or QPixmaps during MIMApplication destruction.
+    // Hence the finalize handler that runs before the d'tor.
+    mPluginsProxyWidget.reset();
+    mPassThruWindow.reset();
+    mRemoteWindow.reset();
+}
+
+bool MImXServerLogic::selfCompositionSupported()
+{
+    return mCompositeExtension.supported(0, 2) && mDamageExtension.supported();
+}
+
+void MImXServerLogic::x11EventFilter(XEvent *ev)
+{
+    handleTransientEvents(ev);
+    handleRemoteWindowEvents(ev);
+    handlePassThruMapEvent(ev);
+}
+
+void MImXServerLogic::handleTransientEvents(XEvent *ev)
+{
+    if (not mRemoteWindow.data()) {
+        return;
+    }
+
+    if (mRemoteWindow->wasIconified(ev) || mRemoteWindow->wasUnmapped(ev)) {
+        qDebug() << "MImX11ServerLogic" << __PRETTY_FUNCTION__
+                 << "Remote window was destroyed or iconified - hiding.";
+
+        Q_EMIT remoteWindowChanged(0);
+        Q_EMIT applicationWindowGone();
+        mRemoteWindow.reset();
+    }
+}
+
+void MImXServerLogic::handleRemoteWindowEvents(XEvent *event)
+{
+    if (not mRemoteWindow.data()) {
+        return;
+    }
+
+    mRemoteWindow->handleEvent(event);
+}
+
+void MImXServerLogic::handlePassThruMapEvent(XEvent *ev)
+{
+    if (ev->type != MapNotify)
+        return;
+
+    if (ev->xmap.window != mPassThruWindow->effectiveWinId())
+        return;
+
+    if (not mRemoteWindow.data()) {
+        qWarning() << __PRETTY_FUNCTION__
+                   << "No remote window found, but passthru window was mapped.";
+        return;
+    }
+
+    mRemoteWindow->resetPixmap();
+}
+
+void MImXServerLogic::applicationFocusChanged(WId newRemoteWinId)
+{
+    if (0 == newRemoteWinId) {
+        return;
+    }
+
+    if (mRemoteWindow.data() && mRemoteWindow->id() == newRemoteWinId) {
+        return;
+    }
+
+    const bool wasRedirected(mRemoteWindow.data() && mRemoteWindow->isRedirected());
+
+    mRemoteWindow.reset(new MImRemoteWindow(newRemoteWinId, this, xOptions));
+    mRemoteWindow->setIMWidget(mPassThruWindow->window());
+
+    connect(mRemoteWindow.data(), SIGNAL(contentUpdated(QRegion)),
+            this,                SLOT(updatePassThruWindow(QRegion)));
+
+    if (wasRedirected) {
+        mRemoteWindow->redirect();
+    }
+
+    Q_EMIT remoteWindowChanged(mRemoteWindow.data());
+}
+
+void MImXServerLogic::setSuppressBackground(bool suppress)
+{
+    mBackgroundSuppressed = suppress;
+}
+
+QWidget *MImXServerLogic::passThruWindow() const
+{
+    return mPassThruWindow.data();
+}
+
+QWidget* MImXServerLogic::pluginsProxyWidget() const
+{
+    return mPluginsProxyWidget.data();
+}
+
+const QPixmap &MImXServerLogic::remoteWindowPixmap()
+{
+    if (not mRemoteWindow.data()
+            || mBackgroundSuppressed
+            || not xOptions.selfComposited) {
+        static const QPixmap empty;
+        return empty;
+    }
+
+    return mRemoteWindow->windowPixmap();
+}
+
+void MImXServerLogic::updatePassThruWindow(const QRegion &region)
+{
+    if (region.isEmpty() || remoteWindowPixmap().isNull()) {
+        qDebug() << "Skipping update request for passthru window.";
+        return;
+    }
+
+    mPassThruWindow->updateFromRemoteWindow(region);
+}
+
+void MImXServerLogic::pluginLoaded()
+{
+    configureWidgetsForCompositing();
+}
+
+void MImXServerLogic::configureWidgetsForCompositing()
+{
+    ConfigureForCompositing visitor(xOptions);
+    visitWidgetHierarchy(visitor, mPassThruWindow.data());
+}
+
+void MImXServerLogic::appOrientationAboutToChange(int toAngle) {
+    mRotationAnimation->appOrientationAboutToChange(toAngle);
+}
+
+void MImXServerLogic::appOrientationChangeFinished(int toAngle) {
+    mRotationAnimation->appOrientationChangeFinished(toAngle);
+}
+
+MImRemoteWindow *MImXServerLogic::remoteWindow() const
+{
+    return mRemoteWindow.data();
+}
+
+void MImXServerLogic::inputPassthrough(const QRegion &region)
+{
+    mPassThruWindow->inputPassthrough(region);
+}
+
+const MImXCompositeExtension& MImXServerLogic::compositeExtension()
+{
+    return mCompositeExtension;
+}
+
+const MImXDamageExtension& MImXServerLogic::damageExtension()
+{
+    return mDamageExtension;
+}
---
+++ src/mimxserverlogic.h
@@ -0,0 +1,102 @@
+/* * This file is part of meego-im-framework *
+ *
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * All rights reserved.
+ * Contact: Nokia Corporation (directui@nokia.com)
+ *
+ * If you have questions regarding the use of this file, please contact
+ * Nokia at directui@nokia.com.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.LGPL included in the packaging
+ * of this file.
+ */
+
+#ifndef MIMX11SERVERLOGIC_H
+#define MIMX11SERVERLOGIC_H
+
+#include "mimabstractserverlogic.h"
+
+#include "mimxextension.h"
+
+typedef union _XEvent XEvent;
+
+struct MImServerXOptions;
+class MImRemoteWindow;
+class MPassThruWindow;
+class MImRotationAnimation;
+
+// Server logic for standalone X11 server
+//
+// For X11 the toplevel window is a MPassThruWindow (managed by MImXServerLogic).
+// It contains a MImPluginsProxyWidget as container for all plugin widgets and a
+// MImRotationAnimation, which is used to display the rotation animation. MImXServerLogic
+// also manages MImRemoteWindows representing the current application window.
+class MImXServerLogic : public MImAbstractServerLogic
+{
+    Q_OBJECT
+public:
+    explicit MImXServerLogic(const MImServerXOptions &options, QObject *parent = 0);
+    virtual ~MImXServerLogic();
+
+    void x11EventFilter(XEvent *ev);
+    bool selfCompositionSupported();
+
+    const MImXCompositeExtension& compositeExtension();
+    const MImXDamageExtension& damageExtension();
+
+    //! Flag that is used to sync between MAbstractInputMethodHost and
+    //! rotation animation in order to capture the VKB without
+    //! self-composited background drawn.
+    void setSuppressBackground(bool suppress);
+
+    QWidget *passThruWindow() const;
+    //! reimpl
+    virtual QWidget *pluginsProxyWidget() const;
+
+    const QPixmap &remoteWindowPixmap();
+
+    //! For unittesting purposes
+    MImRemoteWindow *remoteWindow() const;
+
+public Q_SLOTS:
+    //! reimpl
+    virtual void inputPassthrough(const QRegion &region);
+    virtual void appOrientationAboutToChange(int toAngle);
+    virtual void appOrientationChangeFinished(int toAngle);
+    virtual void applicationFocusChanged(WId remoteWinId);
+    virtual void pluginLoaded();
+
+Q_SIGNALS:
+    //! This signal is emitted when remote window is changed.
+    //! Parameter can be 0 if window is unmapped.
+    void remoteWindowChanged(MImRemoteWindow *newWindow);
+
+private Q_SLOTS:
+    void finalize();
+    void updatePassThruWindow(const QRegion &region);
+
+private:
+    const MImServerXOptions &xOptions;
+
+    void configureWidgetsForCompositing();
+    void handleTransientEvents(XEvent *ev);
+    void handleRemoteWindowEvents(XEvent *ev);
+    void handlePassThruMapEvent(XEvent *ev);
+
+    bool mBackgroundSuppressed;
+
+    MImXCompositeExtension mCompositeExtension;
+    MImXDamageExtension mDamageExtension;
+
+    QScopedPointer<MPassThruWindow> mPassThruWindow;
+    QScopedPointer<QWidget> mPluginsProxyWidget;
+    QScopedPointer<MImRemoteWindow> mRemoteWindow;
+    MImRotationAnimation *mRotationAnimation;
+
+    friend class Ut_PassthroughServer;
+};
+
+#endif // MIMX11SERVERLOGIC_H
