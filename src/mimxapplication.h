030aa5b3339e769c817543840a513836cf21b968
Jon Nordby <jonn@openismus.com>
2012-03-08 15:58:42 +0100

    Remove obsolete code from MImXApplication
    Has been moved to MImXServerLogic

--- src/mimxapplication.h
+++ src/mimxapplication.h
@@ -1,16 +1,9 @@
 #ifndef MIMXAPPLICATION_H
 #define MIMXAPPLICATION_H
 
-#include "mpassthruwindow.h"
-#include "mimremotewindow.h"
-#include "mimxextension.h"
-#include "mimxserverlogic.h"
-
-#include <tr1/functional>
 #include <QApplication>
 
-class MImRotationAnimation;
-struct MImServerXOptions;
+#include "mimxserverlogic.h"
 
 /*! \ingroup maliitserver
  * \brief Application abstraction for X applications.
@@ -19,76 +12,16 @@ class MImXApplication : public QApplication
 {
     Q_OBJECT
 public:
-    //! Walks over widget hierarchy, if used with
-    //! MImXApplication::visitWidgetHierarchy. Return true if children of
-    //! current widget shall be visited, too.
-    typedef std::tr1::function<bool (QWidget *, const MImServerXOptions &)> WidgetVisitor;
 
     MImXApplication(int &argc, char** argv, const MImServerXOptions &options);
     virtual ~MImXApplication();
     static MImXApplication *instance();
 
-    const MImXCompositeExtension& compositeExtension() { return mCompositeExtension; }
-    const MImXDamageExtension& damageExtension() { return mDamageExtension; }
-
-    //! Requires a valid remoteWinId and a valid passThruWindow before it'll
-    //! start to work.
-    //! \sa setTransientHint(), setPassThruWindow();
     bool x11EventFilter(XEvent *ev);
-
-    //! Flag that is used to sync between MAbstractInputMethodHost and
-    //! rotation animation in order to capture the VKB without
-    //! self-composited background drawn.
-    void setSuppressBackground(bool suppress);
-
-    QWidget *passThruWindow() const;
-    QWidget *pluginsProxyWidget() const;
-
-    virtual const QPixmap &remoteWindowPixmap();
-
-    void visitWidgetHierarchy(WidgetVisitor visitor, QWidget *widget);
-
-    //! For unittesting purposes
-    MImRemoteWindow *remoteWindow() const;
 
     MImXServerLogic *serverLogic() const;
 
-public Q_SLOTS:
-    void setTransientHint(WId remoteWinId);
-
-    void configureWidgetsForCompositing();
-
-Q_SIGNALS:
-    //! This signal is emitted when remote window is changed.
-    //! Parameter can be 0 if window is unmapped.
-    void remoteWindowChanged(MImRemoteWindow *newWindow);
-
-    void remoteWindowGone();
-
-private Q_SLOTS:
-    void finalize();
-
-    void updatePassThruWindow(const QRegion &region);
-
-    void appOrientationAboutToChange(int toAngle);
-    void appOrientationChangeFinished(int toAngle);
-
 private:
-    void handleTransientEvents(XEvent *ev);
-    void handleRemoteWindowEvents(XEvent *ev);
-    void handlePassThruMapEvent(XEvent *ev);
-
-    MImXCompositeExtension mCompositeExtension;
-    MImXDamageExtension mDamageExtension;
-
-    bool mBackgroundSuppressed;
-
-    QScopedPointer<MPassThruWindow> mPassThruWindow;
-    QScopedPointer<QWidget> mPluginsProxyWidget;
-    QScopedPointer<MImRemoteWindow> mRemoteWindow;
-    MImRotationAnimation *mRotationAnimation;
-
-    const MImServerXOptions &xOptions;
     QScopedPointer<MImXServerLogic> mServerLogic;
 
     friend class Ut_PassthroughServer;
