5a21e5a260379ddc4fb649a78bc238d4c4d9648b
Jan Arne Petersen <jpetersen@openismus.com>
2012-05-22 10:48:50 +0200

    Use MaliitInputMethod in Gtk+ examples
    Show area-changed signal and maliit_input_method_show and
    maliit_input_method_hide in use.
    
    RevBy: TBD

--- examples/apps/gtk2/exampleapp-gtk.c
+++ examples/apps/gtk2/exampleapp-gtk.c
@@ -16,24 +16,72 @@
 
 #include <gtk/gtk.h>
 
+#include <maliit-glib/maliitinputmethod.h>
+
+static void
+show_button_clicked(GtkButton *button G_GNUC_UNUSED,
+                    gpointer user_data)
+{
+    MaliitInputMethod *input_method = MALIIT_INPUT_METHOD(user_data);
+
+    maliit_input_method_show(input_method);
+}
+
+static void
+hide_button_clicked(GtkButton *button G_GNUC_UNUSED,
+                    gpointer user_data)
+{
+    MaliitInputMethod *input_method = MALIIT_INPUT_METHOD(user_data);
+
+    maliit_input_method_hide(input_method);
+}
+
+static void
+input_method_area_changed(MaliitInputMethod *input_method G_GNUC_UNUSED,
+                          int x, int y, int width, int height,
+                          gpointer user_data)
+{
+    GtkLabel *label = GTK_LABEL(user_data);
+    gchar *formatted_string;
+
+    if (width == 0 || height == 0)
+        formatted_string = g_strdup("Empty Input Method area");
+    else
+        formatted_string = g_strdup_printf("Input Method area: [%d, %d, %d, %d]", x, y, width, height);
+
+    gtk_label_set_text(label, formatted_string);
+
+    g_free(formatted_string);
+}
+
 gint
 main(gint    argc,
      gchar **argv)
 {
     GtkWidget *window, *vbox;
-    GtkWidget *button, *entry, *text_view;
+    GtkWidget *show_button, *hide_button, *entry, *text_view, *label;
+    MaliitInputMethod *input_method;
 
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Maliit GTK+ example");
 
-    // Clicking the button will steal focus from the text entry, thus hiding
-    // the virtual keyboard:
-    button = gtk_button_new_with_label("Hide keyboard");
+    input_method = maliit_input_method_new();
+
+    show_button = gtk_button_new_with_label("Show keyboard");
+    g_signal_connect(show_button, "clicked",
+                     G_CALLBACK(show_button_clicked), input_method);
+    hide_button = gtk_button_new_with_label("Hide keyboard");
+    g_signal_connect(hide_button, "clicked",
+                     G_CALLBACK(hide_button_clicked), input_method);
     entry = gtk_entry_new();
     text_view = gtk_text_view_new();
     gtk_widget_set_size_request(text_view, 150, 200);
+
+    label = gtk_label_new("Empty Input Method area");
+    g_signal_connect(input_method, "area-changed",
+                     G_CALLBACK(input_method_area_changed), label);
 
 #if GTK_MAJOR_VERSION >= 3
     vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
@@ -41,9 +89,11 @@ main(gint    argc,
     vbox = gtk_vbox_new(FALSE, 10);
 #endif
 
-    gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), show_button, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), hide_button, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), text_view, TRUE, TRUE, 0); /* Expands */
+    gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     g_signal_connect(G_OBJECT(window), "delete-event",
@@ -51,5 +101,8 @@ main(gint    argc,
     gtk_widget_show_all(window);
 
     gtk_main();
+
+    g_object_unref(input_method);
+
     return 0;
 }
