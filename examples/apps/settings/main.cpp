da5163a51238eb06902c961b562a6067434026e7
Krzesimir Nowak <krnowak@openismus.com>
2012-06-21 15:28:55 +0200

    Fix license in settings example.
    RevBy: Jon Nordby

--- examples/apps/settings/main.cpp
+++ examples/apps/settings/main.cpp
@@ -1,12 +1,23 @@
-/* * This file is part of meego-im-framework *
+/* This file is part of Maliit framework
  *
  * Copyright (C) 2012 Mattia Barbon <mattia@develer.com>
  *
+ * Contact: maliit-discuss@lists.maliit.org
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License version 2.1 as published by the Free Software Foundation
- * and appearing in the file LICENSE.LGPL included in the packaging
- * of this file.
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the licence, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #include "mainwindow.h"
