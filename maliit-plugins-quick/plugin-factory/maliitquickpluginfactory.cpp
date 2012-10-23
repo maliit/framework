673b5e066935ae6b493c63cc934bc7a9e96d8108
Michael Hasselmann <michaelh@openismus.com>
2012-10-23 21:04:11 +0200

    Make QtQuick plugin factory work for Qt5
    Re-enables loading of (pure) QML plugins, such as the Nemo Keyboard. There seem
    to be rendering issues where the screen is not properly updated, making the
    Nemo Keyboard completely unusable with Qt5, for now.

--- maliit-plugins-quick/plugin-factory/maliitquickpluginfactory.cpp
+++ maliit-plugins-quick/plugin-factory/maliitquickpluginfactory.cpp
@@ -59,4 +59,6 @@ Maliit::Plugins::InputMethodPlugin * MaliitQuickPluginFactory::create(const QStr
     return new MyPlugin(file);
 }
 
+#if QT_VERSION < 0x050000
 Q_EXPORT_PLUGIN2(MaliitQuickPluginFactory, MaliitQuickPluginFactory)
+#endif
