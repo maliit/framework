33a4e1938f33c0efff770119a66e7c54f6222517
Krzesimir Nowak <krnowak@openismus.com>
2013-03-15 12:10:10 +0100

    Add Wayland surface handling to window group.
    RevBy: TBD

--- src/windowgroup.cpp
+++ src/windowgroup.cpp
@@ -17,6 +17,14 @@
 #include <QVector>
 #include <QWindow>
 
+#ifdef HAVE_WAYLAND
+#include <QGuiApplication>
+#include <qpa/qplatformnativeinterface.h>
+
+#include <wayland-client.h>
+#include "wayland-input-method-client-protocol.h"
+#endif // HAVE_WAYLAND
+
 #include "windowgroup.h"
 
 namespace Maliit
@@ -45,25 +53,170 @@ WindowData::WindowData(QWindow *window, Maliit::Position position)
       m_position(position)
 {}
 
+#ifdef HAVE_WAYLAND
+input_panel_surface_position maliitToWestonPosition (Maliit::Position position)
+{
+    switch (position) {
+    case Maliit::PositionCenterBottom:
+        return INPUT_PANEL_SURFACE_POSITION_CENTER_BOTTOM;
+    default:
+        qWarning() << "Weston only supports center bottom position for top-level surfaces.";
+
+        return INPUT_PANEL_SURFACE_POSITION_CENTER_BOTTOM;
+    }
+}
+#endif // HAVE_WAYLAND
+
 } // anonymous namespace
 
 class WindowGroupPrivate
 {
 public:
     WindowGroupPrivate();
+    ~WindowGroupPrivate();
 
     bool containsWindow(QWindow *window);
+    Maliit::Position getPosition(QWindow *window, bool &found);
+    QWindow *getTopLevelWindow();
+
+#ifdef HAVE_WAYLAND
+    void handleRegistryGlobal(uint32_t name,
+                              const char *interface,
+                              uint32_t version);
+    void handleRegistryGlobalRemove(uint32_t name);
+    void handleOutputGeometry(int32_t x,
+                              int32_t y,
+                              int32_t physical_width,
+                              int32_t physical_height,
+                              int32_t subpixel,
+                              const char *make,
+                              const char *model,
+                              int32_t transform);
+    void handleOutputMode(uint32_t flags,
+                          int32_t width,
+                          int32_t height,
+                          int32_t refresh);
+
+    bool setupInputSurface(QWindow *window, bool avoid_crash = false);
+
+#endif // HAVE_WAYLAND
 
     QVector<WindowData> m_window_list;
     QRegion m_last_im_area;
     bool m_active;
+#ifdef HAVE_WAYLAND
+    struct wl_registry *m_registry;
+    struct wl_output *m_output;
+    struct input_panel *m_panel;
+#endif // HAVE_WAYLAND
 };
 
+#ifdef HAVE_WAYLAND
+namespace {
+
+void registryGlobal(void *data,
+                    wl_registry *registry,
+                    uint32_t name,
+                    const char *interface,
+                    uint32_t version)
+{
+    qDebug() << __PRETTY_FUNCTION__;
+    WindowGroupPrivate *d = static_cast<WindowGroupPrivate *>(data);
+
+    Q_UNUSED(registry);
+    d->handleRegistryGlobal(name, interface, version);
+}
+
+void registryGlobalRemove(void *data,
+                          wl_registry *registry,
+                          uint32_t name)
+{
+    qDebug() << __PRETTY_FUNCTION__;
+    WindowGroupPrivate *d = static_cast<WindowGroupPrivate *>(data);
+
+    Q_UNUSED(registry);
+    d->handleRegistryGlobalRemove(name);
+}
+
+const wl_registry_listener maliit_registry_listener = {
+    registryGlobal,
+    registryGlobalRemove
+};
+
+void outputGeometry(void *data,
+                    struct wl_output *output,
+                    int32_t x,
+                    int32_t y,
+                    int32_t physical_width,
+                    int32_t physical_height,
+                    int32_t subpixel,
+                    const char *make,
+                    const char *model,
+                    int32_t transform) {
+    qDebug() << __PRETTY_FUNCTION__;
+    WindowGroupPrivate *d = static_cast<WindowGroupPrivate *>(data);
+
+    Q_UNUSED(output);
+    d->handleOutputGeometry(x, y, physical_width, physical_height, subpixel, make,
+                            model, transform);
+}
+
+void outputMode(void *data,
+                struct wl_output *output,
+                uint32_t flags,
+                int32_t width,
+                int32_t height,
+                int32_t refresh) {
+    qDebug() << __PRETTY_FUNCTION__;
+    WindowGroupPrivate *d = static_cast<WindowGroupPrivate *>(data);
+
+    Q_UNUSED(output);
+    d->handleOutputMode(flags, width, height, refresh);
+}
+
+const wl_output_listener maliit_output_listener = {
+    outputGeometry,
+    outputMode
+};
+
+} // unnamed namespace
+
+#endif // HAVE_WAYLAND
+
 WindowGroupPrivate::WindowGroupPrivate()
-    : m_window_list(),
-      m_last_im_area(),
-      m_active(false)
-{}
+    : m_window_list()
+    , m_last_im_area()
+    , m_active(false)
+#ifdef HAVE_WAYLAND
+    , m_registry(0)
+    , m_output(0)
+    , m_panel(0)
+#endif // HAVE_WAYLAND
+{
+#ifdef HAVE_WAYLAND
+    wl_display *display = static_cast<wl_display *>(QGuiApplication::platformNativeInterface()->nativeResourceForIntegration("display"));
+    if (!display) {
+        qCritical() << __PRETTY_FUNCTION__ << "Failed to get a display.";
+        return;
+    }
+    m_registry = wl_display_get_registry(display);
+    wl_registry_add_listener(m_registry, &maliit_registry_listener, this);
+    // QtWayland will do dispatching for us.
+#endif // HAVE_WAYLAND
+}
+
+WindowGroupPrivate::~WindowGroupPrivate()
+{
+    if (m_panel) {
+        input_panel_destroy (m_panel);
+    }
+    if (m_output) {
+        wl_output_destroy (m_output);
+    }
+    if (m_registry) {
+        wl_registry_destroy (m_registry);
+    }
+}
 
 bool WindowGroupPrivate::containsWindow(QWindow *window)
 {
@@ -75,6 +228,135 @@ bool WindowGroupPrivate::containsWindow(QWindow *window)
 
     return false;
 }
+
+Maliit::Position WindowGroupPrivate::getPosition(QWindow *window, bool &found)
+{
+    Q_FOREACH (const WindowData &data, m_window_list) {
+        if (data.m_window == window) {
+            found = true;
+            return data.m_position;
+        }
+    }
+
+    found = false;
+    return Maliit::PositionCenterBottom;
+}
+
+QWindow *WindowGroupPrivate::getTopLevelWindow()
+{
+    Q_FOREACH (const WindowData &data, m_window_list) {
+        if (data.m_window and not data.m_window->parent()) {
+            return data.m_window;
+        }
+    }
+
+    return 0;
+}
+
+#ifdef HAVE_WAYLAND
+void WindowGroupPrivate::handleRegistryGlobal(uint32_t name,
+                                              const char *interface,
+                                              uint32_t version)
+{
+    Q_UNUSED(version);
+
+    qDebug() << __PRETTY_FUNCTION__ << "Name:" << name << "Interface:" << interface;
+    if (!strcmp(interface, "input_panel")) {
+        m_panel = static_cast<input_panel *>(wl_registry_bind(m_registry, name, &input_panel_interface, 1));
+
+        QWindow *window = getTopLevelWindow ();
+
+        if (window) {
+            setupInputSurface(window, true);
+        }
+    } else if (!strcmp(interface, "wl_output")) {
+        if (m_output) {
+            // Ignoring the event - we already have an output.
+            // TODO: Later we will want to store those outputs as well
+            // to be able to choose where the input method plugin is
+            // shown.
+            return;
+        }
+
+        m_output = static_cast<wl_output *>(wl_registry_bind(m_registry, name, &wl_output_interface, 1));
+        wl_output_add_listener (m_output, &maliit_output_listener, this);
+    }
+}
+
+void WindowGroupPrivate::handleRegistryGlobalRemove(uint32_t name)
+{
+    qDebug() << __PRETTY_FUNCTION__ << "Name:" << name;
+}
+
+void WindowGroupPrivate::handleOutputGeometry(int32_t x,
+                                              int32_t y,
+                                              int32_t physical_width,
+                                              int32_t physical_height,
+                                              int32_t subpixel,
+                                              const char *make,
+                                              const char *model,
+                                              int32_t transform)
+{
+    qDebug() << __PRETTY_FUNCTION__
+             << "x:" << x
+             << "y:" << y
+             << "physical width:" << physical_width
+             << "physical height:" << physical_height
+             << "subpixel:" << subpixel
+             << "make:" << make
+             << "model:" << model
+             << "transform:" << transform;
+}
+
+void WindowGroupPrivate::handleOutputMode(uint32_t flags,
+                                          int32_t width,
+                                          int32_t height,
+                                          int32_t refresh)
+{
+    qDebug() << __PRETTY_FUNCTION__
+             << "flags:" << flags
+             << "width:" << width
+             << "height:" << height
+             << "refresh:" << refresh;
+}
+
+bool WindowGroupPrivate::setupInputSurface(QWindow *window,
+                                           bool avoid_crash)
+{
+    struct wl_surface *surface = avoid_crash ? 0 : static_cast<struct wl_surface *>(QGuiApplication::platformNativeInterface()->nativeResourceForWindow("surface", window));
+
+    if (not surface) {
+        if (avoid_crash) {
+            qDebug() << "Creating surface to avoid crash in QtWayland";
+        } else {
+            qDebug() << "No wl_surface, calling create.";
+        }
+        qDebug() << __PRETTY_FUNCTION__ << "WId:" << window->winId();
+        window->create();
+    }
+
+    surface = static_cast<struct wl_surface *>(QGuiApplication::platformNativeInterface()->nativeResourceForWindow("surface", window));
+    qDebug() << __PRETTY_FUNCTION__ << "WId:" << window->winId();
+    if (surface) {
+        input_panel_surface *ip_surface = input_panel_get_input_panel_surface(m_panel, surface);
+        bool found = false;
+        Maliit::Position position (getPosition(window, found));
+
+        if (found) {
+            input_panel_surface_position weston_position = maliitToWestonPosition (position);
+
+            input_panel_surface_set_toplevel(ip_surface, weston_position);
+            return true;
+        } else {
+            qCritical() << __PRETTY_FUNCTION__ << "Looks like that window is not on ourlist.";
+        }
+    } else {
+        qDebug() << __PRETTY_FUNCTION__ << "Still no surface, giving up.";
+    }
+    return false;
+}
+
+#endif // HAVE_WAYLAND
 
 WindowGroup::WindowGroup()
     : d_ptr(new WindowGroupPrivate)
@@ -114,6 +396,12 @@ void WindowGroup::setupWindow(QWindow *window, Maliit::Position position)
 
     if (window) {
         if (not d->containsWindow(window)) {
+            QWindow *parent = window->parent ();
+
+            if (parent and not d->containsWindow(parent)) {
+                qWarning () << "Plugin is misbehaving - tried to register a window with yet-unregistered parent!";
+                return;
+            }
             d->m_window_list.append (WindowData(window, position));
 
             window->setFlags (Qt::Dialog |
@@ -132,6 +420,11 @@ void WindowGroup::setupWindow(QWindow *window, Maliit::Position position)
                      this, SLOT (updateInputMethodArea()));
             connect (window, SIGNAL (yChanged(int)),
                      this, SLOT (updateInputMethodArea()));
+#ifdef HAVE_WAYLAND
+            if (not parent and d->m_panel) {
+                d->setupInputSurface(window);
+            }
+#endif // HAVE_WAYLAND
             updateInputMethodArea();
         }
     }
