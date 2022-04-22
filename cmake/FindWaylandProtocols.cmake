5809266da219d4a86383911905db6d1597de9b13
Arjen Hiemstra <ahiemstra@heimr.nl>
2022-04-22 12:03:54 +0200

    Replace FindWaylandProtocols.cmake with the version from ECM
    It uses pkgconfig correctly and allows using things like
    CMAKE_PREFIX_PATH to point at different installations.

--- cmake/FindWaylandProtocols.cmake
+++ cmake/FindWaylandProtocols.cmake
@@ -1,39 +1,38 @@
-#.rst:
-# FindWaylandProtocols
-# -------
+# SPDX-FileCopyrightText: 2019 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
 #
-# Find wayland protocol description files
-#
-# Try to find wayland protocol files. The following values are defined
-#
-# ::
-#
-#   WAYLANDPROTOCOLS_FOUND         - True if wayland protocol files are available
-#   WAYLANDPROTOCOLS_PATH          - Path to wayland protocol files
-#
-#=============================================================================
-# Copyright (c) 2015 Jari Vetoniemi
-#
-# Distributed under the OSI-approved BSD License (the "License");
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
+# SPDX-License-Identifier: BSD-3-Clause
+
+#[=======================================================================[.rst:
+FindWaylandProtocols
+--------------------
+
+Try to find wayland-protocols on a Unix system.
+
+This will define the following variables:
+
+``WaylandProtocols_FOUND``
+    True if (the requested version of) wayland-protocols is available
+``WaylandProtocols_VERSION``
+    The version of wayland-protocols
+``WaylandProtocols_DATADIR``
+    The wayland protocols data directory
+#]=======================================================================]
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(PKG_wayland_protocols QUIET wayland-protocols)
+
+set(WaylandProtocols_VERSION ${PKG_wayland_protocols_VERSION})
+pkg_get_variable(WaylandProtocols_DATADIR wayland-protocols pkgdatadir)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(WaylandProtocols
+    FOUND_VAR WaylandProtocols_FOUND
+    REQUIRED_VARS WaylandProtocols_DATADIR
+    VERSION_VAR WaylandProtocols_VERSION
+)
 
 include(FeatureSummary)
 set_package_properties(WaylandProtocols PROPERTIES
-        URL "https://cgit.freedesktop.org/wayland/wayland-protocols"
-        DESCRIPTION "Wayland protocol development")
-
-unset(WAYLANDPROTOCOLS_PATH)
-
-find_package(PkgConfig)
-if (PKG_CONFIG_FOUND)
-    execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} --variable=pkgdatadir wayland-protocols
-            OUTPUT_VARIABLE WAYLANDPROTOCOLS_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
-endif ()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(WaylandProtocols DEFAULT_MSG WAYLANDPROTOCOLS_PATH)
-mark_as_advanced(WAYLANDPROTOCOLS_PATH)
+    DESCRIPTION "Specifications of extended Wayland protocols"
+    URL "https://wayland.freedesktop.org/"
+)
